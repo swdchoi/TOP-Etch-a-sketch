30e2d2949ff9f1e46163ff3ffc39524d803c254d
swdchoi <sungwonchoi224@gmail.com>
2024-11-22 18:27:26 +0800

    16x16 and 32x32 working with mouseover red color

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const canv = document.querySelector("#canv");
-const start = document.querySelector("#start");
-let boxNum16by16 = 256;
-let stbyst = false;
+const stbyst = document.querySelector("#start");
+const threetwo = document.querySelector("#threetwo");
+const cleartable = document.querySelector("#clear");
 
-const bigbox16by16 = () => {
-if(stbyst == true){
-alert("already at 16 x 16!")
-} else if (stbyst == false){
-    for (let i = boxNum16by16 - 1 ; i >= 0; i--) {
+let boxNum = 0;
+
+
+const colorChange = (a) => {
+    a.style.backgroundColor = "red";
+    console.log("hi");
+}
+
+const gridMade = () => {
+const boxesId = document.querySelectorAll(".drawbox");
+
+boxesId.forEach((i) =>
+i.addEventListener("mouseover", e => colorChange(i))
+)
+}
+
+const bigbox = (a) => {
+
+    boxNum = a * a;
+    for (let i = boxNum - 1 ; i >= 0; i--) {
     const box = document.createElement("div");
     box.classList.add("drawbox");
+    box.style.height = `${480/a}px`
+    box.style.width = `${480/a}px`;
+    box.id = i;
     canv.appendChild(box);
-    }
 
-    stbyst = true;
+    gridMade();
 }
 }
 
-start.addEventListener("click", (e) => {bigbox16by16()});
+const clear = () => {
+    for (let i = boxNum - 1 ; i >= 0; i--) {
+        const box = document.getElementById(`${i}`);
+        canv.removeChild(box);
+        }
 
+};
+
+cleartable.addEventListener("click", (e) => {clear()});
+threetwo.addEventListener("click", (e) => {bigbox(32)});
+stbyst.addEventListener("click", (e) => {bigbox(16)});
