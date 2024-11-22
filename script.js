2f9b2b46135f4ec453bcb67c90276272eb0fd048
swdchoi <sungwonchoi224@gmail.com>
2024-11-22 11:56:48 +0800

    create new box inside canvas

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const canv = document.querySelector(canv);
+const canv = document.querySelector("#canv");
+const start = document.querySelector("#start");
+let boxNum16by16 = 256;
+let stbyst = false;
+
+const bigbox16by16 = () => {
+if(stbyst == true){
+alert("already at 16 x 16!")
+} else if (stbyst == false){
+    for (let i = boxNum16by16 - 1 ; i >= 0; i--) {
+    const box = document.createElement("div");
+    box.classList.add("drawbox");
+    canv.appendChild(box);
+    }
+
+    stbyst = true;
+}
+}
+
+start.addEventListener("click", (e) => {bigbox16by16()});
 
